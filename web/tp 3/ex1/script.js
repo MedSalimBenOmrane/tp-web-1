d77b1d59ba1e80886e12f354ddfb0df1033e4a03
MedSalimBenOmrane <94930813+MedSalimBenOmrane@users.noreply.github.com>
2022-04-03 11:34:11 +0100

    Add files via upload

---
+++ web/tp 3/ex1/script.js
@@ -0,0 +1,39 @@
+function getdiff(diff)
+{
+document.f.rep.value=diff;
+}
+
+function tester(num)
+{
+    console.log(alea);
+    if(document.f.difficulté.value==0)
+        {alert ("choisir un niveau de defficulte !!");
+    return 0;}
+    coup=document.f.rep.value;
+    coup = coup -1;
+
+if (num.value == alea) {
+    test = confirm("Vous avez gagné \nNouvelle partie ?");
+}
+else if (num.value != alea) {
+    document.f.rep.value = coup;
+}
+else if (coup <1 ) {
+    test = confirm("T'as perdu !\n tu doit trouver : " + alea + "\nNouvelle partie ?");
+}
+
+     if (test) {
+        window.location.reload();
+    }
+    else {
+    window.close();
+    }
+
+}
+
+
+const alea = Math.round(Math.random() * 9) + 1;
+
+
+
+
